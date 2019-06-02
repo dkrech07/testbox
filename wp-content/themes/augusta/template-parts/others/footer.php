5d0318b921d81977f7cddb8244b7d59cc0b40f48
dkrech07 <dkrech07@gmail.com>
2019-06-02 23:05:41 +0500

    footer changes

--- wp-content/themes/augusta/template-parts/others/footer.php
+++ wp-content/themes/augusta/template-parts/others/footer.php
@@ -1,15 +1,28 @@
 <footer id="colophon" class="site-footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12 text-center">
+
+			<div class="col-md-8 text-left">
 				<div class="site-info">
-					<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'augusta' ), 'augusta', '<a href="https://templatic.com/" rel="nofollow">Templatic</a>' );
-					?>
+					<nav id="site-navigation" class="main-navigation">
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						) );
+						?>
+					</nav><!-- #site-navigation -->
 				</div><!-- .site-info -->
 			</div>
 			<!-- /.col-md-12 text-center -->
+
+			<div class="col-md-4 text-right">
+				<div class="site-info">
+					<p>2019 © testbox</p>
+				</div><!-- .site-info -->
+			</div>
+			<!-- /.col-md-12 text-center -->
+
 		</div>
 		<!-- /.row -->
 	</div>
